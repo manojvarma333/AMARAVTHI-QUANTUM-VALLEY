9e6cc0ac1348ee5fa59c2ceaa9870891d41a3ef4
Manoj <2300033136@kluniversity.in>
2025-08-28 11:43:26 +0530

    Initial Commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,13 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/quantum-jobs-dashboard/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
   },
 });
